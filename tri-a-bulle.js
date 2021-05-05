be9aa068003cc7249d084ac6c848767cc0229e14
laurentveyrat <57811353+laurentveyrat@users.noreply.github.com>
2021-05-05 10:36:11 +0200

    Implémentation du tri à bulle

---
+++ tri-a-bulle.js
@@ -0,0 +1,20 @@
+// const tab = [4, 5, 1, 3, 2]
+
+let tabSaisi = process.argv.slice(2)
+tabSaisi = tabSaisi.map(chiffre => parseInt(chiffre))
+
+function triDecroissant(tableau) {
+    let tabTrie = tableau.slice()
+    for (let i = (tabTrie.length - 1); i > 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            if (tabTrie[j] > tabTrie[j + 1]) {
+                let tmp = tabTrie[j + 1]
+                tabTrie[j + 1] = tabTrie[j]
+                tabTrie[j] = tmp
+            }
+        }
+    }
+    return tabTrie.reverse()
+}
+
+console.log(triDecroissant(tabSaisi).join(' '))
